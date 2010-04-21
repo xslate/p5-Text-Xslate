688879ba7335274fa6a937c82bbbddd7d06f5594
gfx <gfuji@cpan.org>
2010-04-21 15:09:20 +0900

    Fix pods

--- lib/Text/Xslate.pm
+++ lib/Text/Xslate.pm
@@ -476,13 +476,13 @@ L<HTML::Template::Pro>
 
 =head1 AUTHOR
 
-Goro Fuji (gfx) E<lt>gfuji(at)cpan.orgE<gt>
+Fuji, Goro (gfx) E<lt>gfuji(at)cpan.orgE<gt>
 
 =head1 LICENSE AND COPYRIGHT
 
-Copyright (c) 2010, Goro Fuji (gfx). All rights reserved.
+Copyright (c) 2010, Fuji, Goro (gfx). All rights reserved.
 
 This library is free software; you can redistribute it and/or modify
-it under the same terms as Perl itself. See L<perlartistic> for details.
+it under the same terms as Perl itself.
 
 =cut
