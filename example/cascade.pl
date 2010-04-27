8dcbfa799591bc135650cdbc8a786ec6c4780485
Goro Fuji <gfuji@cpan.org>
2010-04-28 02:53:19 +0900

    Cache examples

--- example/cascade.pl
+++ example/cascade.pl
@@ -24,7 +24,6 @@ my @blog_entries = map{ BlogEntry->new($_) } (
 my $tx = Text::Xslate->new(
     file  => 'child.tx',
     path  => ["$Bin/template"],
-    cache => 0,
 );
 
 print $tx->render('child.tx', { blog_entries => \@blog_entries });
