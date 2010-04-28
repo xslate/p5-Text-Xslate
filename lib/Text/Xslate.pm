b71e98f91f4949b9a52c345ca9a041520a2a1465
gfx <gfuji@cpan.org>
2010-04-28 21:52:55 +0900

    Checking in changes prior to tagging of version 0.1000.
    Changelog diff is:
    
    diff --git a/Changes b/Changes
    index 3a09b02..a2d4338 100644
    --- a/Changes
    +++ b/Changes
    @@ -1,5 +1,10 @@
     Revision history for Perl extension Text::Xslate
    
    +0.1000 Wed Apr 28 21:50:29 2010
    +    - First non-dev release!
    +    - Change the default path to ["."]
    +    - Fix a macrocall issue
    +
     0.001_09 Wed Apr 28 18:01:42 2010
         - New keywords: cascade, macro, block, before, around and after
         - Add template cascading

--- lib/Text/Xslate.pm
+++ lib/Text/Xslate.pm
@@ -6,7 +6,7 @@ use 5.010_000;
 use strict;
 use warnings;
 
-our $VERSION = '0.001_09';
+our $VERSION = '0.1000';
 
 use XSLoader;
 XSLoader::load(__PACKAGE__, $VERSION);
@@ -247,7 +247,7 @@ Text::Xslate - High performance template engine
 
 =head1 VERSION
 
-This document describes Text::Xslate version 0.001_09.
+This document describes Text::Xslate version 0.1000.
 
 =head1 SYNOPSIS
 
