6287aec9f512181c04bcc3d7df8c5b2acfa8c5a5
gfx <gfuji@cpan.org>
2010-04-30 14:33:50 +0900

    Checking in changes prior to tagging of version 0.1004.
    Changelog diff is:
    
    diff --git a/Changes b/Changes
    index 51e3026..ec3ffa4 100644
    --- a/Changes
    +++ b/Changes
    @@ -1,5 +1,8 @@
     Revision history for Perl extension Text::Xslate
    
    +0.1004 Fri Apr 30 14:32:47 2010
    +    - Fix an over-optimizing issue (revealed by Template::Benchmark)
    +
     0.1003 Fri Apr 30 12:30:21 2010
         - Disable leaktrace testing temporarily

--- lib/Text/Xslate.pm
+++ lib/Text/Xslate.pm
@@ -6,7 +6,7 @@ use 5.010_000;
 use strict;
 use warnings;
 
-our $VERSION = '0.1003';
+our $VERSION = '0.1004';
 
 use XSLoader;
 XSLoader::load(__PACKAGE__, $VERSION);
@@ -240,7 +240,7 @@ Text::Xslate - High performance template engine
 
 =head1 VERSION
 
-This document describes Text::Xslate version 0.1003.
+This document describes Text::Xslate version 0.1004.
 
 =head1 SYNOPSIS
 
