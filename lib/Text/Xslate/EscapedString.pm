7b6767e6f4aff8a0cbed3b4461029dfb0434f7bf
Fuji, Goro <gfuji@cpan.org>
2010-05-04 22:05:06 +0900

    Docs

--- lib/Text/Xslate/EscapedString.pm
+++ lib/Text/Xslate/EscapedString.pm
@@ -1,5 +1,5 @@
 package Text::Xslate::EscapedString;
-# the package in implemented intentionally in Text::Xslate
+# the package is implemented intentionally in Text::Xslate
 use Text::Xslate;
 1;
 __END__
@@ -7,5 +7,11 @@ __END__
 =head1 NAME
 
 Text::Xslate::EscapedString - The escaped string representation
+
+=jead1 DESCRIPTION
+
+EscapedString is strings marked as escaped so that Xslate does not escape them.
+
+Note that you cannot extend this class.
 
 =cut
