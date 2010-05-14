9311360a8c039c67cb1f1fcfa4d010cadaa41f52
Fuji, Goro <gfuji@cpan.org>
2010-05-14 14:54:28 +0900

    Checking in changes prior to tagging of version 0.1015.
    Changelog diff is:
    
    diff --git a/Changes b/Changes
    index 6f1694b..d735a55 100644
    --- a/Changes
    +++ b/Changes
    @@ -2,6 +2,11 @@ Revision history for Perl extension Text::Xslate
    
     NOTE: The builtin methods are comming soon!
    
    +0.1015 Fri May 14 14:52:52 2010
    +    [BUG FIXES]
    +    - Fix literal_to_value(), which could break values (reported by xaicron)
    +    - Missing prerequisites: MouseX::Getopt (reported by xaicron)
    +
     0.1014 Fri May 14 13:05:17 2010
         [NEW FEATURES]
         - Now Xslate works without XS (makamaka)

--- lib/Text/Xslate.pm
+++ lib/Text/Xslate.pm
@@ -4,7 +4,7 @@ use 5.010_000;
 use strict;
 use warnings;
 
-our $VERSION = '0.1014';
+our $VERSION = '0.1015';
 
 use parent qw(Exporter);
 our @EXPORT_OK = qw(escaped_string html_escape);
@@ -346,7 +346,7 @@ Text::Xslate - High performance template engine
 
 =head1 VERSION
 
-This document describes Text::Xslate version 0.1014.
+This document describes Text::Xslate version 0.1015.
 
 =head1 SYNOPSIS
 
--- lib/Text/Xslate/PP.pm
+++ lib/Text/Xslate/PP.pm
@@ -4,7 +4,7 @@ package Text::Xslate::PP;
 use 5.008;
 use strict;
 
-our $VERSION = '0.1014';
+our $VERSION = '0.1015';
 
 use Carp ();
 
@@ -352,7 +352,7 @@ Text::Xslate::PP - Yet another Text::Xslate runtime in pure Perl
 
 =head1 VERSION
 
-This document describes Text::Xslate::PP version 0.1014.
+This document describes Text::Xslate::PP version 0.1015.
 
 =head1 DESCRIPTION
 
