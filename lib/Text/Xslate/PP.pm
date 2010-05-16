7695f177056b86cf19db8aadb43682d3fbc83b69
Fuji, Goro <gfuji@cpan.org>
2010-05-16 14:01:47 +0900

    Checking in changes prior to tagging of version 0.1016.
    Changelog diff is:
    
    diff --git a/Changes b/Changes
    index d735a55..f9801e5 100644
    --- a/Changes
    +++ b/Changes
    @@ -1,6 +1,11 @@
     Revision history for Perl extension Text::Xslate
    
    -NOTE: The builtin methods are comming soon!
    +0.1016 Sun May 16 13:59:08 2010
    +    [NEW FEATURES]
    +    - Support --pp (and --xs) option in Makefile.PL
    +    - Support autoboxing; currently supported methods are:
    +        For array: size(), join(), reverse()
    +        For hash:  keys(), values(), kv() (and all the array methods)
    
     0.1015 Fri May 14 14:52:52 2010
         [BUG FIXES]

--- lib/Text/Xslate/PP.pm
+++ lib/Text/Xslate/PP.pm
@@ -4,7 +4,7 @@ package Text::Xslate::PP;
 use 5.008;
 use strict;
 
-our $VERSION = '0.1015';
+our $VERSION = '0.1016';
 
 use Carp ();
 
@@ -352,7 +352,7 @@ Text::Xslate::PP - Yet another Text::Xslate runtime in pure Perl
 
 =head1 VERSION
 
-This document describes Text::Xslate::PP version 0.1015.
+This document describes Text::Xslate::PP version 0.1016.
 
 =head1 DESCRIPTION
 
