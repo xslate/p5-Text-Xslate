123514b93acfeb5d0ade47414f34558c11c7cbda
Fuji, Goro <gfuji@cpan.org>
2010-05-18 11:05:31 +0900

    Support hex/oct/binary literals (e.g. 0xCAFE, 0755, 0b1010)

--- lib/Text/Xslate/Util.pm
+++ lib/Text/Xslate/Util.pm
@@ -15,7 +15,17 @@ use Carp ();
 my $dquoted = qr/" (?: \\. | [^"\\] )* "/xms; # " for poor editors
 my $squoted = qr/' (?: \\. | [^'\\] )* '/xms; # ' for poor editors
 our $STRING  = qr/(?: $dquoted | $squoted )/xms;
-our $NUMBER  = qr/(?: [+-]? [0-9][0-9_]* (?: \. [0-9_]+)? )/xms;
+our $NUMBER  = qr/ [+-]? (?:
+        (?: [1-9][0-9_]* (?: \. [0-9_]+)? ) # int or real
+        |
+        (?: 0 (?:
+            (?: [0-7]+ )           # octal
+            |
+            (?: x [0-9a-fA-F]+) # hex
+            |
+            (?: b [01]+ )       # binary
+        ))
+    )/xms;
 
 our $DEBUG;
 $DEBUG //= $ENV{XSLATE} // '';
@@ -30,11 +40,19 @@ sub literal_to_value {
     my($value) = @_;
     return undef if not defined $value;
 
-    if($value =~ s/"(.*)"/$1/){
+    if($value =~ s/"(.*)"/$1/xms){
         $value =~ s/\\(.)/$esc2char{$1} || $1/xmseg;
     }
-    elsif($value =~ s/'(.*)'/$1/) {
-        $value =~ s/\\(['\\])/$1/g; # ' for poor editors
+    elsif($value =~ s/'(.*)'/$1/xms) {
+        $value =~ s/\\(['\\])/$1/xmsg; # ' for poor editors
+    }
+    else { # number
+        if($value =~ s/\A ([+-]?) (?= 0.)//xms) {
+            $value = ($1 eq '-' ? -1 : +1) * oct($value); # can grok hex and binary
+        }
+        else {
+            $value =~ s/_//xmsg;
+        }
     }
 
     return $value;
