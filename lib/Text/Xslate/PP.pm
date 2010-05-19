bcd191b8a467c7a054bdbc687df6242dce10361e
Fuji, Goro <gfuji@cpan.org>
2010-05-19 15:36:42 +0900

    Checking in changes prior to tagging of version 0.1018.
    Changelog diff is:
    
    diff --git a/Changes b/Changes
    index ac03450..ab99080 100644
    --- a/Changes
    +++ b/Changes
    @@ -1,5 +1,12 @@
     Revision history for Perl extension Text::Xslate
    
    +0.1018 Wed May 19 15:34:47 2010
    +    [CHANGES]
    +    - Use Any::Moose (lestrrat)
    +
    +    [BUG FIXES]
    +    - There was several issues on Mouse::PurePerl
    +
     0.1017 Tue May 18 14:35:30 2010
         [NEW FEATURES]
         - Support hex/oct/binary literals (e.g. 0xCAFE)

--- lib/Text/Xslate/PP.pm
+++ lib/Text/Xslate/PP.pm
@@ -4,7 +4,7 @@ package Text::Xslate::PP;
 use 5.008;
 use strict;
 
-our $VERSION = '0.1017';
+our $VERSION = '0.1018';
 
 use Carp ();
 
@@ -355,7 +355,7 @@ Text::Xslate::PP - Yet another Text::Xslate runtime in pure Perl
 
 =head1 VERSION
 
-This document describes Text::Xslate::PP version 0.1017.
+This document describes Text::Xslate::PP version 0.1018.
 
 =head1 DESCRIPTION
 
