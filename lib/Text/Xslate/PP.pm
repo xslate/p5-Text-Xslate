f0fdd6ca2ea1fbd22dacb93a9c6e9dff734f0a0f
gfx <gfuji@cpan.org>
2010-05-30 19:53:22 +0900

    Checking in changes prior to tagging of version 0.1024.
    Changelog diff is:

--- lib/Text/Xslate/PP.pm
+++ lib/Text/Xslate/PP.pm
@@ -4,7 +4,7 @@ package Text::Xslate::PP;
 use 5.008;
 use strict;
 
-our $VERSION = '0.1023';
+our $VERSION = '0.1024';
 
 use Carp ();
 
@@ -380,7 +380,7 @@ Text::Xslate::PP - Yet another Text::Xslate runtime in pure Perl
 
 =head1 VERSION
 
-This document describes Text::Xslate::PP version 0.1023.
+This document describes Text::Xslate::PP version 0.1024.
 
 =head1 DESCRIPTION
 
