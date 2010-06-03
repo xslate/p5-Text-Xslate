f21c88d0ac1b6cc0a92f4fd01e4458d4c96ea913
makamaka <makamaka.donzoko@gmail.com>
2010-06-03 22:39:51 +0900

    fixed _check_logic ternary part for push opcode

--- lib/Text/Xslate/PP/Booster.pm
+++ lib/Text/Xslate/PP/Booster.pm
@@ -743,21 +743,24 @@ sub _check_logic {
 
         my $has_else_block = ($else_block_end >= $else_block_start);
 
-        my $last_opname = $ops->[ $i + $addr + $oparg - 1 ]->[0]; # check print or move_to_sb
-
         my $st_1st = $self->_spawn_child->_convert_opcode(
             [ @{ $ops }[ $if_block_start .. $if_block_end ] ]
         );
 
+        my $last_op = $ops->[ $i + $addr + $oparg - 1 ]; # check for ternary
+
         # treat as ternary
-        if ( $has_else_block and $last_opname and ( $last_opname eq 'print' or $last_opname eq 'move_to_sb' ) ) {
+        if ( $has_else_block and $last_op and $last_op->[0] =~ /^(?:print|move_to_sb|push)$/ ) {
 
             for (  $else_block_start .. $else_block_end ) { # 2
                 $self->stash->{ proc }->{ $_ }->{ skip } = 1; # skip
             }
 
+            # when last op is 'push', must add pushmark
             my $st_2nd = $self->_spawn_child->_convert_opcode( # add $last_code for nested ternary
-                [ @{ $ops }[ $else_block_start .. $else_block_end ], [ $last_opname ] ]
+                $last_op->[0] eq 'push' 
+                    ? [ [ 'pushmark' ], @{ $ops }[ $else_block_start .. $else_block_end ], $last_op ]
+                    : [ @{ $ops }[ $else_block_start .. $else_block_end ], $last_op ]
             );
 
         $self->sa( sprintf(  <<'CODE', $self->sa, _rm_tailed_lf( $st_1st->sa ), _rm_tailed_lf( $st_2nd->sa ) ) );
