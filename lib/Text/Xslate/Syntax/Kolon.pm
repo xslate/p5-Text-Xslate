1551a84736daa86fc03736a763fa121fe92fa1a0
gfx <gfuji@cpan.org>
2010-06-05 11:40:21 +0900

    Update docs

--- lib/Text/Xslate/Syntax/Kolon.pm
+++ lib/Text/Xslate/Syntax/Kolon.pm
@@ -162,19 +162,12 @@ Supported iterator elements are C<index :Int>, C<count :Int>,
 C<body : ArrayRef>, C<size : Int>, C<max :Int>, C<is_first :Bool>,
 and C<is_last :Bool>, C<peek_next :Any>, C<peek_prev :Any>.
 
-C<while> loops are also supported to iterate database-handle-like objects.
+C<while> loops are also supported in the same semantics as Perl's:
 
-    : # $obj must be an iteratable object
+    : # $obj might be an iteratable object
     : while $dbh.fetch() -> $item {
         [<: $item.field :>]
     : }
-
-Note that C<while> is B<not the same as Perl's>. In fact, the above Xslate
-C<while> code is the same as the following Perl C<while> code:
-
-    while(defined(my $item = $dbh->fetch())) {
-        ...
-    }
 
 =head2 Conditional statements
 
@@ -196,7 +189,7 @@ C<if-else>:
         $var is 1 .. 10
     : }
 
-Note that C<if> doesn't require parens, so the above code is okay:
+Note that C<if> doesn't require parens, so the following code is okay:
 
     : if ($var + 10) == 20 { } # OK
 
