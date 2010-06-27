300de30238e3454af26f40e04dc5597d9162e426
gfx <gfuji@cpan.org>
2010-06-27 17:48:53 +0900

    Checking in changes prior to tagging of version 0.1037.
    Changelog diff is:
    
    diff --git a/Changes b/Changes
    index 527863d..eea2bb5 100644
    --- a/Changes
    +++ b/Changes
    @@ -1,9 +1,12 @@
     Revision history for Perl extension Text::Xslate
    
    -0.1037
    +0.1037 Sun Jun 27 17:37:57 2010
         [BUG FIXES]
         - Template cascading broke the file name of error messages
    
    +    [OTHERS]
    +    - Internal refactoring
    +
     0.1036 Wed Jun 23 20:05:44 2010
         [CHANGES]
         - Change line_start, tag_start and tag_end to take string tokens,

--- lib/Text/Xslate.pm
+++ lib/Text/Xslate.pm
@@ -4,7 +4,7 @@ use 5.008_001;
 use strict;
 use warnings;
 
-our $VERSION = '0.1036';
+our $VERSION = '0.1037';
 
 use Carp       ();
 use File::Spec ();
@@ -473,7 +473,7 @@ Text::Xslate - High performance template engine
 
 =head1 VERSION
 
-This document describes Text::Xslate version 0.1036.
+This document describes Text::Xslate version 0.1037.
 
 =head1 SYNOPSIS
 
