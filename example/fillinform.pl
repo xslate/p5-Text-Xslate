781e17502f7dc30398c709bd91eedd345d4f35eb
gfx <gfuji@cpan.org>
2010-06-28 18:51:05 +0900

    FillInForm::Lite 1.09 supports function interface

--- example/fillinform.pl
+++ example/fillinform.pl
@@ -2,25 +2,9 @@
 use strict;
 
 use Text::Xslate;
-BEGIN {
-    eval{ require HTML::FillInForm::Lite::Compat };
-}
-use HTML::FillInForm;
-
-sub fillinform {
-    my($q) = @_;
-
-    return sub {
-        my($html) = @_;
-        return HTML::FillInForm->fill(\$html, $q);
-    };
-}
 
 my $tx  = Text::Xslate->new(
-    cache => 0,
-    function => {
-        fillinform => \&fillinform,
-    },
+    module => [qw(HTML::FillInForm::Lite) => [qw(fillinform)]],
 );
 
 print $tx->render_string(<<'T', { q => { foo => "<filled value>" } });
