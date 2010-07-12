1135490e4dd9063ae5139d4a3641215e673c4e2e
gfx <gfuji@cpan.org>
2010-07-12 18:05:04 +0900

    Add benchmark to sprintf()

--- benchmark/interpolate.pl
+++ benchmark/interpolate.pl
@@ -20,11 +20,21 @@ my $mt = build_mt("Hello, <?= \$_[0]->{lang} ?> world!\n" x $n);
 
 my $subst_tmpl = qq{Hello, %lang% world!\n} x $n;
 
+my $sprintf_tmpl = qq{Hello, %1\$s world!\n} x $n;
+
 my $vars = {
     lang => 'Template',
 };
 
-$tx->render(undef, $vars) eq $mt->($vars) or die $tx->render($vars);
+{
+    use Test::More;
+    plan tests => 3;
+    my $x = $tx->render(undef, $vars);
+    is $mt->($vars), $x, 'Text::MicroTemplate';
+    (my $o = $subst_tmpl) =~ s/%(\w+)%/$vars->{$1}/g;
+    is $o, $x, 's///g';
+    is sprintf($sprintf_tmpl, $vars->{lang}), $x, 'sprintf';
+}
 
 # suppose PSGI response body
 
@@ -33,13 +43,17 @@ cmpthese -1 => {
         my $body = [$tx->render(undef, $vars)];
         return;
     },
-    mt => sub {
+    TMT => sub {
         my $body = [$mt->($vars)];
         return;
     },
     's///g' => sub {
         my $body = [$subst_tmpl];
         $body->[0] =~ s/%(\w+)%/$vars->{$1}/g;
+        return;
+    },
+    'sprintf' => sub {
+        my $body = [ sprintf $sprintf_tmpl, $vars->{lang} ];
         return;
     },
 };
