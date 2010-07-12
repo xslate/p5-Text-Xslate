6cc8e953b09c55efc8c7e01a796d1647695cddfe
gfx <gfuji@cpan.org>
2010-07-12 17:51:49 +0900

    Checking in changes prior to tagging of version 0.1044.
    Changelog diff is:
    
    diff --git a/Changes b/Changes
    index f8a4d0c..1a7b6cd 100644
    --- a/Changes
    +++ b/Changes
    @@ -1,10 +1,10 @@
     Revision history for Perl extension Text::Xslate
    
    -0.1044
    +0.1044 Mon Jul 12 17:50:35 2010
         [BUG FIXES]
         - Underscores in bare words (i.e. left-hand side of the operator '=>')
           were accidentally removed.
    -    - path => \%vpath did not work.
    +    - There were several issues on path => \%vpath
    
         [NEW FESTURES]
         - $array.reduce(-> $x, $y { ... }) method

--- lib/Text/Xslate/PP.pm
+++ lib/Text/Xslate/PP.pm
@@ -3,7 +3,7 @@ package Text::Xslate::PP;
 use 5.008_001;
 use strict;
 
-our $VERSION = '0.1043';
+our $VERSION = '0.1044';
 
 BEGIN{
     $ENV{XSLATE} = ($ENV{XSLATE} || '') . '[pp]';
@@ -522,7 +522,7 @@ Text::Xslate::PP - Yet another Text::Xslate runtime in pure Perl
 
 =head1 VERSION
 
-This document describes Text::Xslate::PP version 0.1043.
+This document describes Text::Xslate::PP version 0.1044.
 
 =head1 DESCRIPTION
 
