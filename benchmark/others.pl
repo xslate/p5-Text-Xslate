c7afa6eca5c411855624173f910306dfdb549514
tokuhirom <tokuhirom@gmail.com>
2010-07-30 09:48:17 +0900

    fixed typo in benchmarking script

--- benchmark/others.pl
+++ benchmark/others.pl
@@ -25,7 +25,7 @@ if(!($tmpl eq 'list' or $tmpl eq 'include')) {
 use Config; printf "Perl/%vd %s\n", $^V, $Config{archname};
 
 my $has_tcs = eval q{ use Text::ClearSilver 0.10.5.4; 1 };
-warn "Text::CelarSilver is not available ($@)\n" if $@;
+warn "Text::ClearSilver is not available ($@)\n" if $@;
 
 my $has_mst = $tmpl eq 'list' && $try_mst && eval q{ use MobaSiF::Template; 1 };
 warn "MobaSif::Template is not available ($@)\n" if $@;
