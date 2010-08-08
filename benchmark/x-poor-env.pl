dbac7108f4409bf2faefd4a60fb3eaf3b11ec5c3
gfx <gfuji@cpan.org>
2010-08-08 17:37:48 +0900

    Split benchmark/others.pl into 2 files: x-rich-env.pl (XS) and x-poor-env.pl (PP)

---
+++ benchmark/x-poor-env.pl
@@ -0,0 +1,143 @@
+#!perl
+# For poor environment, e.g. CGI applications without XS
+
+use strict;
+use warnings;
+
+use Getopt::Long;
+
+use Test::More;
+use Benchmark qw(:all);
+use FindBin qw($Bin);
+
+use Template;
+use HTML::Template;
+use Text::MicroTemplate::Extended;
+
+use Config; printf "Perl/%vd %s\n", $^V, $Config{archname};
+
+GetOptions(
+    'booster'    => \my $pp_booster,
+
+    'size=i'     => \my $n,
+    'template=s' => \my $tmpl,
+);
+
+$tmpl = 'include' if not defined $tmpl;
+$n    = 100       if not defined $n;
+
+$ENV{XSLATE} = $pp_booster ? 'pp=booster' : 'pp=opcode';
+$ENV{MOUSE_PUREPERL} = 1;
+$Template::Config::STASH = 'Template::Stash'; # Instead of Stash::XS
+
+require Text::Xslate;
+
+foreach my $mod(qw(
+    Text::Xslate
+    Template
+    HTML::Template
+    Text::MicroTemplate
+    Text::MicroTemplate::Extended
+)){
+    print $mod, '/', $mod->VERSION, "\n" if $mod->VERSION;
+}
+
+my $path = "$Bin/template";
+
+my $vars = {
+    data => [ ({
+            title    => "FOO",
+            author   => "BAR",
+            abstract => "BAZ",
+        }) x $n
+   ],
+};
+
+TEST: {
+    my $tx = Text::Xslate->new(
+        path       => [$path],
+        cache_dir  =>  '.xslate_cache',
+        cache      => 2,
+    );
+    my $mt = Text::MicroTemplate::Extended->new(
+        include_path => [$path],
+        cache        => 2,
+    );
+    my $tt = Template->new(
+        INCLUDE_PATH => [$path],
+        COMPILE_EXT  => '.out',
+    );
+
+    my $ht = HTML::Template->new(
+        path              => [$path],
+        filename          => "$tmpl.ht",
+        case_sensitive    => 1,
+        die_on_bad_params => 0,
+        double_file_cache => 1,
+        file_cache_dir    => '.xslate_cache',
+    );
+
+    my $expected = $tx->render("$tmpl.tx", $vars);
+    $expected =~ s/\n+/\n/g;
+
+
+    plan tests => 3;
+
+    $tt->process("$tmpl.tt", $vars, \my $out) or die $tt->error;
+    $out =~ s/\n+/\n/g;
+    is $out, $expected, 'TT: Template-Toolkit (with file caching)';
+
+    $out = $mt->render_file($tmpl, $vars);
+    $out =~ s/\n+/\n/g;
+    is $out, $expected, 'MT: Text::MicroTemplate';
+
+    $ht->param($vars);
+    $out = $ht->output();
+    $out =~ s/\n+/\n/g;
+    is $out, $expected, 'HT: HTML::Template (with file caching)';
+}
+
+print "Benchmarks with '$tmpl' (datasize=$n)\n";
+cmpthese -1 => {
+    Xslate => sub {
+        my $tx = Text::Xslate->new(
+            path       => [$path],
+            cache_dir  =>  '.xslate_cache',
+            cache      => 2,
+        );
+        my $body = $tx->render("$tmpl.tx", $vars);
+        return;
+    },
+    MT => sub {
+        my $mt = Text::MicroTemplate::Extended->new(
+            include_path => [$path],
+            cache        => 2,
+        );
+        my $body = $mt->render_file($tmpl, $vars);
+        return;
+    },
+    TT => sub {
+        my $tt = Template->new(
+            INCLUDE_PATH => [$path],
+            COMPILE_EXT  => '.out',
+        );
+        my $body;
+        $tt->process("$tmpl.tt", $vars, \$body) or die $tt->error;
+        return;
+    },
+    HT => sub {
+        my $ht = HTML::Template->new(
+            path              => [$path],
+            filename          => "$tmpl.ht",
+            case_sensitive    => 1,
+            die_on_bad_params => 0,
+            double_file_cache => 1,
+            file_cache_dir    => '.xslate_cache',
+        );
+
+        $ht->param($vars);
+        my $body = $ht->output();
+        return;
+    },
+};
+
--- benchmark/x-rich-env.pl
+++ benchmark/x-rich-env.pl
@@ -1,42 +1,32 @@
 #!perl
-# templates: benchmark/template/list.*
+# For rich environment, e.g. Persistent PSGI applications with XS
 
 use strict;
 use warnings;
 
+use Text::Xslate;
 use Text::MicroTemplate::Extended;
 use Template;
+
 use Getopt::Long;
 
 use Test::More;
 use Benchmark qw(:all);
 use FindBin qw($Bin);
+use Config; printf "Perl/%vd %s\n", $^V, $Config{archname};
 
 GetOptions(
     'mst' => \my $try_mst,
-    'pp'  => \my $pp,
-    'booster' => \my $pp_booster,
+
+    'size=i'     => \my $n,
+    'template=s' => \my $tmpl,
 );
-
-
-if ($pp) {
-    print "testing with PP\n";
-    $Template::Config::STASH = 'Template::Stash';
-    $ENV{XSLATE} = $pp_booster ? 'pp=booster' : 'pp';
-    $ENV{MOUSE_PUREPERL} = 1;
-}
 
 require Text::Xslate;
 
-my $tmpl   = !Scalar::Util::looks_like_number($ARGV[0]) && shift(@ARGV);
-   $tmpl ||= 'list';
-my $n      = shift(@ARGV) || 100;
+$tmpl = 'include' if not defined $tmpl;
+$n    = 100       if not defined $n;
 
-if(!($tmpl eq 'list' or $tmpl eq 'include')) {
-    die "$0 [list | include] [n]\n";
-}
-
-use Config; printf "Perl/%vd %s\n", $^V, $Config{archname};
 
 my $has_tcs = eval q{ use Text::ClearSilver 0.10.5.4; 1 };
 warn "Text::ClearSilver is not available ($@)\n" if $@;
@@ -47,12 +37,14 @@ warn "MobaSiF::Template is not available ($@)\n" if $try_mst && $@;
 my $has_htp = eval q{ use HTML::Template::Pro; 1 };
 warn "HTML::Template::Pro is not available ($@)\n" if $@;
 
-my $has_ht = eval q{ use HTML::Template; 1 };
-warn "HTML::Template is not available ($@)\n" if $@;
-
 foreach my $mod(qw(
-    Text::Xslate Text::MicroTemplate Template
-    Text::ClearSilver MobaSiF::Template HTML::Template::Pro
+    Text::Xslate
+    Text::MicroTemplate
+    Text::MicroTemplate::Extended
+    Template
+    Text::ClearSilver
+    MobaSiF::Template
+    HTML::Template::Pro
 )){
     print $mod, '/', $mod->VERSION, "\n" if $mod->VERSION;
 }
@@ -96,16 +88,6 @@ if($has_htp) {
     );
 }
 
-my $ht;
-if($has_ht) {
-    $ht = HTML::Template->new(
-        path           => [$path],
-        filename       => "$tmpl.ht",
-        case_sensitive => 1,
-        die_on_bad_params => 0,
-    );
-}
-
 my $vars = {
     data => [ ({
             title    => "FOO",
@@ -123,7 +105,6 @@ my $vars = {
     $tests++ if $has_tcs;
     $tests++ if $has_mst;
     $tests++ if $has_htp;
-    $tests++ if $has_ht;
     plan tests => $tests;
 
     $tt->process("$tmpl.tt", $vars, \my $out) or die $tt->error;
@@ -152,13 +133,6 @@ my $vars = {
         $out =~ s/\n+/\n/g;
         is $out, $expected, 'HTP: HTML::Template::Pro';
     }
-
-    if($has_ht) {
-        $ht->param($vars);
-        $out = $ht->output();
-        $out =~ s/\n+/\n/g;
-        is $out, $expected, 'HT: HTML::Template';
-    }
 }
 
 print "Benchmarks with '$tmpl' (datasize=$n)\n";
@@ -177,30 +151,23 @@ cmpthese -1 => {
         return;
     },
 
-    (!$pp && $has_tcs) ? (
+    $has_tcs ? (
         TCS => sub {
             my $body;
             $tcs->process("$tmpl.cs", $vars, \$body);
             return;
         },
     ) : (),
-    (!$pp && $has_mst) ? (
+    $has_mst ? (
         MST => sub {
             my $body = MobaSiF::Template::insert($mst_bin, $vars);
             return;
         },
     ) : (),
-    (!$pp && $has_htp) ? (
+    $has_htp ? (
         HTP => sub {
             $htp->param($vars);
             my $body = $htp->output();
-            return;
-        },
-    ) : (),
-    $has_ht ? (
-        HT => sub {
-            $ht->param($vars);
-            my $body = $ht->output();
             return;
         },
     ) : (),
