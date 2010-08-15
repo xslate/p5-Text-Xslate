220dbe1a391a083bd53d77ad4274568a58f5dfa4
gfx <gfuji@cpan.org>
2010-08-15 18:32:34 +0900

    Checking in changes prior to tagging of version 0.1057.
    Changelog diff is:
    
    diff --git a/Changes b/Changes
    index 4f097d1..5891192 100644
    --- a/Changes
    +++ b/Changes
    @@ -1,5 +1,17 @@
     Revision history for Perl extension Text::Xslate
    
    +0.1057 Sun Aug 15 18:28:32 2010
    +    [BUG FIXES]
    +    - Subroutines that localize $SIG{__WARN__} or $SIG{__DIE__} could make
    +      Perl interpreter panic (reported by tokuhirom)
    +    - Invalid expressions could produce insane errors including infinite loop
    +      (issue/13, reported by y)
    +    - A Win32 problem in tests might be fixed
    +
    +    [FEATURES]
    +    - Experimental support for PP::Compiler which make PP::Booster faster
    +      (makamaka)
    +
     0.1056 Tue Aug 10 14:28:55 2010
         [BUG FIXES]
         - The rendering methods ignored signals

--- lib/Text/Xslate.pm
+++ lib/Text/Xslate.pm
@@ -4,7 +4,7 @@ use 5.008_001;
 use strict;
 use warnings;
 
-our $VERSION = '0.1056';
+our $VERSION = '0.1057';
 
 use Carp        ();
 use File::Spec  ();
@@ -504,7 +504,7 @@ Text::Xslate - High performance template engine
 
 =head1 VERSION
 
-This document describes Text::Xslate version 0.1056.
+This document describes Text::Xslate version 0.1057.
 
 =head1 SYNOPSIS
 
