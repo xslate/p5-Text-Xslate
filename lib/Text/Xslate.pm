5f8e22f0ae11d0ac7e4cfe9bb27eb9d5b609cba0
gfx <gfuji@cpan.org>
2010-08-17 18:00:59 +0900

    Checking in changes prior to tagging of version 0.1058.
    Changelog diff is:
    
    diff --git a/Changes b/Changes
    index 399967e..7b38f30 100644
    --- a/Changes
    +++ b/Changes
    @@ -1,6 +1,6 @@
     Revision history for Perl extension Text::Xslate
    
    -1.058
    +0.1058 Tue Aug 17 17:59:53 2010
         [CHANGES]
         - Deprecate defined() method, which is provided only for compatibility
           with TT, but the behaviour might not be exceptable. That is,

--- lib/Text/Xslate.pm
+++ lib/Text/Xslate.pm
@@ -4,7 +4,7 @@ use 5.008_001;
 use strict;
 use warnings;
 
-our $VERSION = '0.1057';
+our $VERSION = '0.1058';
 
 use Carp        ();
 use File::Spec  ();
@@ -507,7 +507,7 @@ Text::Xslate - High performance template engine
 
 =head1 VERSION
 
-This document describes Text::Xslate version 0.1057.
+This document describes Text::Xslate version 0.1058.
 
 =head1 SYNOPSIS
 
--- lib/Text/Xslate/PP.pm
+++ lib/Text/Xslate/PP.pm
@@ -3,7 +3,7 @@ package Text::Xslate::PP;
 use 5.008_001;
 use strict;
 
-our $VERSION = '0.1057';
+our $VERSION = '0.1058';
 
 BEGIN{
     $ENV{XSLATE} = ($ENV{XSLATE} || '') . '[pp]';
@@ -700,7 +700,7 @@ Text::Xslate::PP - Yet another Text::Xslate runtime in pure Perl
 
 =head1 VERSION
 
-This document describes Text::Xslate::PP version 0.1057.
+This document describes Text::Xslate::PP version 0.1058.
 
 =head1 DESCRIPTION
 
