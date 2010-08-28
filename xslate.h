565f8786dd145f8f1ff07d14d92c105b9d6837b0
gfx <gfuji@cpan.org>
2010-08-28 18:30:21 +0900

    Split common stuff

--- xslate.h
+++ xslate.h
@@ -1,49 +1,6 @@
 /* xslate.h */
-#ifdef __cplusplus
-extern "C" {
-#endif
+#include "perlxs.h"
 
-#define PERL_NO_GET_CONTEXT /* we want efficiency */
-#include <EXTERN.h>
-
-#include <perl.h>
-#define NO_XSLOCKS /* for exceptions */
-#include <XSUB.h>
-
-#ifdef __cplusplus
-} /* extern "C" */
-#endif
-
-#include "ppport.h"
-
-/* portability stuff */
-
-#ifndef STATIC_INLINE /* from 5.13.4 */
-#   if defined(__GNUC__) || defined(__cplusplus__) || (defined(__STDC_VERSION__) && (__STDC_VERSION__ >= 199901L))
-#       define STATIC_INLINE static inline
-#   else
-#       define STATIC_INLINE static
-#   endif
-#endif /* STATIC_INLINE */
-
-#ifndef __attribute__format__
-#define __attribute__format__(a,b,c) /* nothing */
-#endif
-
-#ifndef LIKELY /* they are just a compiler's hint */
-#define LIKELY(x)   (x)
-#define UNLIKELY(x) (x)
-#endif
-
-#ifndef newSVpvs_share
-#define newSVpvs_share(s) Perl_newSVpvn_share(aTHX_ STR_WITH_LEN(s), 0U)
-#endif
-
-#if PERL_BCDVERSION < 0x5008005
-#define LooksLikeNumber(x) (SvOK(x) && looks_like_number(x))
-#else
-#define LooksLikeNumber(x) looks_like_number(x)
-#endif
 
 /* xslate stuff */
 
