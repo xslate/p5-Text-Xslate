f4ffdf4ed690e827065791d67d87eb34845c32b9
gfx <gfuji@cpan.org>
2010-09-09 19:28:54 +0900

    Checking in changes prior to tagging of version 0.2003.
    Changelog diff is:
    
    diff --git a/Changes b/Changes
    index 60e0cc1..45fe292 100644
    --- a/Changes
    +++ b/Changes
    @@ -1,5 +1,11 @@
     Revision history for Perl extension Text::Xslate
    
    +0.2003 Thu Sep  9 19:25:08 2010
    +    [BUG FIXES]
    +    * Fix a misuse of test functions. This problem is revealed
    +      by Test::Builder2.
    +    * Workaround MAGIC problems with html_escape($1)
    +
     0.2002 Tue Sep  7 15:23:49 2010
         [FEATURES]
         * Performance improvement (thanks to @kazuho)

--- lib/Text/Xslate.pm
+++ lib/Text/Xslate.pm
@@ -4,7 +4,7 @@ use 5.008_001;
 use strict;
 use warnings;
 
-our $VERSION = '0.2002';
+our $VERSION = '0.2003';
 
 use Carp        ();
 use File::Spec  ();
@@ -512,7 +512,7 @@ Text::Xslate - Scalable template engine for Perl5
 
 =head1 VERSION
 
-This document describes Text::Xslate version 0.2002.
+This document describes Text::Xslate version 0.2003.
 
 =head1 SYNOPSIS
 
