cafb314d95c15752a9d31c5086b7bb9e9b429215
gfx <gfuji@cpan.org>
2010-09-10 13:52:51 +0900

    Checking in changes prior to tagging of version 0.2004.
    Changelog diff is:
    
    diff --git a/Changes b/Changes
    index 45fe292..4f97399 100644
    --- a/Changes
    +++ b/Changes
    @@ -1,5 +1,9 @@
     Revision history for Perl extension Text::Xslate
    
    +0.2004 Fri Sep 10 13:50:08 2010
    +    [BUG FIXES]
    +    * Workaround test failure about 'too long arguments for exec()'
    +
     0.2003 Thu Sep  9 19:25:08 2010
         [BUG FIXES]
         * Fix a misuse of test functions. This problem is revealed

--- lib/Text/Xslate/PP.pm
+++ lib/Text/Xslate/PP.pm
@@ -3,7 +3,7 @@ package Text::Xslate::PP;
 use 5.008_001;
 use strict;
 
-our $VERSION = '0.2003';
+our $VERSION = '0.2004';
 
 BEGIN{
     $ENV{XSLATE} = ($ENV{XSLATE} || '') . '[pp]';
@@ -626,7 +626,7 @@ Text::Xslate::PP - Yet another Text::Xslate runtime in pure Perl
 
 =head1 VERSION
 
-This document describes Text::Xslate::PP version 0.2003.
+This document describes Text::Xslate::PP version 0.2004.
 
 =head1 DESCRIPTION
 
