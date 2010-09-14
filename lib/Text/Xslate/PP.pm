54f144e9fb47a7e2a4cc8264947f29f2ef478e0b
gfx <gfuji@cpan.org>
2010-09-14 11:52:05 +0900

    Checking in changes prior to tagging of version 0.2006.
    Changelog diff is:
    
    diff --git a/Changes b/Changes
    index bf72378..add9ba9 100644
    --- a/Changes
    +++ b/Changes
    @@ -1,5 +1,10 @@
     Revision history for Perl extension Text::Xslate
    
    +0.2006 Tue Sep 14 11:46:33 2010
    +    [BUF FIXES]
    +    * Single-quote literals parsed wrong, when contain paired double q (Mons Anderson)
    +    * Infinite recursion during _fold_constants in case of warnings (Mons Anderson)
    +
     0.2005 Mon Sep 13 14:44:22 2010
         [BUG FIXES]
         * Workaround a new deprecated warnings for qw(...)

--- lib/Text/Xslate/PP.pm
+++ lib/Text/Xslate/PP.pm
@@ -3,7 +3,7 @@ package Text::Xslate::PP;
 use 5.008_001;
 use strict;
 
-our $VERSION = '0.2005';
+our $VERSION = '0.2006';
 
 BEGIN{
     $ENV{XSLATE} = ($ENV{XSLATE} || '') . '[pp]';
@@ -626,7 +626,7 @@ Text::Xslate::PP - Yet another Text::Xslate runtime in pure Perl
 
 =head1 VERSION
 
-This document describes Text::Xslate::PP version 0.2005.
+This document describes Text::Xslate::PP version 0.2006.
 
 =head1 DESCRIPTION
 
