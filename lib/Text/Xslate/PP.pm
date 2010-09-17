0ce644ceb75cc81aeafda3a8a7c382207e228cbb
gfx <gfuji@cpan.org>
2010-09-17 12:16:05 +0900

    Checking in changes prior to tagging of version 0.2007.
    Changelog diff is:
    
    diff --git a/Changes b/Changes
    index add9ba9..72973a9 100644
    --- a/Changes
    +++ b/Changes
    @@ -1,5 +1,9 @@
     Revision history for Perl extension Text::Xslate
    
    +0.2007 Fri Sep 17 12:12:50 2010
    +    [BUG FIXES]
    +    * Resolve RT #61359 (Sam Graham): Taint flag broke templates
    +
     0.2006 Tue Sep 14 11:46:33 2010
         [BUF FIXES]
         * Single-quote literals parsed wrong, when contain paired double q (Mons Anderson)

--- lib/Text/Xslate/PP.pm
+++ lib/Text/Xslate/PP.pm
@@ -3,7 +3,7 @@ package Text::Xslate::PP;
 use 5.008_001;
 use strict;
 
-our $VERSION = '0.2006';
+our $VERSION = '0.2007';
 
 BEGIN{
     $ENV{XSLATE} = ($ENV{XSLATE} || '') . '[pp]';
@@ -626,7 +626,7 @@ Text::Xslate::PP - Yet another Text::Xslate runtime in pure Perl
 
 =head1 VERSION
 
-This document describes Text::Xslate::PP version 0.2006.
+This document describes Text::Xslate::PP version 0.2007.
 
 =head1 DESCRIPTION
 
