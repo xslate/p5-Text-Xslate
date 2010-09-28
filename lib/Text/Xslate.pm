82b8c6a08a33778c70bcffd43b83a2fec1b12845
Fuji, Goro <gfuji@cpan.org>
2010-09-28 16:43:49 +0900

    Checking in changes prior to tagging of version 0.2008_02.
    Changelog diff is:
    
    diff --git a/Changes b/Changes
    index f3b7507..8eb7606 100644
    --- a/Changes
    +++ b/Changes
    @@ -1,13 +1,13 @@
     Revision history for Perl extension Text::Xslate
    
    -0.2008_02
    +0.2008_02 Tue Sep 28 16:42:21 2010
         [BUG FIXES]
    -    * Special variables $/ and $\ broke caches
    -    * Developpers' releases produced warnings about versions
    +    * Cache mechanism was affected by the special variables $/ and $\
    +    * Developpers' releases might produced warnings about versions
    
         [CHANGES]
         * Upgrading Text::Xslate no longer forces to purge cached templates.
    -      Instead, the internal bytecode version (1.0) is used.
    +      Instead, the internal bytecode version (1.0, currentl) is used.
    
     0.2008_01 Wed Sep 22 17:18:53 2010
         [CHANGES]

--- lib/Text/Xslate.pm
+++ lib/Text/Xslate.pm
@@ -4,7 +4,7 @@ use 5.008_001;
 use strict;
 use warnings;
 
-our $VERSION = '0.2008_01';
+our $VERSION = '0.2008_02';
 
 use Carp              ();
 use File::Spec        ();
@@ -506,7 +506,7 @@ Text::Xslate - Scalable template engine for Perl5
 
 =head1 VERSION
 
-This document describes Text::Xslate version 0.2008_01.
+This document describes Text::Xslate version 0.2008_02.
 
 =head1 SYNOPSIS
 
