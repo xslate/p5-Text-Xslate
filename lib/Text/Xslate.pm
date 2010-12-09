1e2de8809b96deb4086e07be64d1422ada08d096
Fuji, Goro <gfuji@cpan.org>
2010-12-09 17:23:04 +0900

    Checking in changes prior to tagging of version 0.3001.
    Changelog diff is:
    
    diff --git a/Changes b/Changes
    index 3d5174b..36ece52 100644
    --- a/Changes
    +++ b/Changes
    @@ -1,5 +1,10 @@
     Revision history for Perl extension Text::Xslate
    
    +0.3001 2010-12-09 17:18:14
    +    [BUG FIXES]
    +    * Fix `module => \@bridges` that importing of bridge modules
    +      incorrectly cleared the function mapping (reported by lestrrat)
    +
     0.3000 2010-11-23 15:19:04
         [CHANGES]
         * `option => undef` passes the option as is, which allows

--- lib/Text/Xslate.pm
+++ lib/Text/Xslate.pm
@@ -4,7 +4,7 @@ use 5.008_001;
 use strict;
 use warnings;
 
-our $VERSION = '0.3000';
+our $VERSION = '0.3001';
 
 use Carp              ();
 use File::Spec        ();
@@ -521,7 +521,7 @@ Text::Xslate - Scalable template engine for Perl5
 
 =head1 VERSION
 
-This document describes Text::Xslate version 0.3000.
+This document describes Text::Xslate version 0.3001.
 
 =head1 SYNOPSIS
 
