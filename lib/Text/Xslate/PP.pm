df3479162f549118eec52677068ae2b47e0327c9
Fuji, Goro <gfuji@cpan.org>
2010-12-16 16:22:08 +0900

    Checking in changes prior to tagging of version 0.3002.
    Changelog diff is:

--- lib/Text/Xslate/PP.pm
+++ lib/Text/Xslate/PP.pm
@@ -3,7 +3,7 @@ package Text::Xslate::PP;
 use 5.008_001;
 use strict;
 
-our $VERSION = '0.3001';
+our $VERSION = '0.3002';
 $VERSION =~ s/_//; # for developpers versions
 
 BEGIN{
@@ -629,7 +629,7 @@ Text::Xslate::PP - Yet another Text::Xslate runtime in pure Perl
 
 =head1 VERSION
 
-This document describes Text::Xslate::PP version 0.3001.
+This document describes Text::Xslate::PP version 0.3002.
 
 =head1 DESCRIPTION
 
