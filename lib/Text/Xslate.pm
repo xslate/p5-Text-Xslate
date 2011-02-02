0a765bd483d9b742434550dd6633ce83ebfb910e
Fuji, Goro <gfuji@cpan.org>
2011-02-02 11:57:15 +0900

    Checking in changes prior to tagging of version 1.0001.
    Changelog diff is:
    
    diff --git a/Changes b/Changes
    index 2df1fcb..6c5332b 100644
    --- a/Changes
    +++ b/Changes
    @@ -1,5 +1,12 @@
     Revision history for Perl extension Text::Xslate
    
    +1.0001 2011-02-02 11:50:43
    +    [BUG FIXES]
    +    * Template caches was not purged correctly when functions were added
    +      (thanks to @tokuhirom for reporting it)
    +    * The 'super' keyword didn't work in template components
    +      (thanks to @shiba_yu36 for reporting it)
    +
     1.0000 2011-01-07 16:08:44
         This is the version 1.0, which means "very stable". Although some features are planned
         to impement in the near future, the basic features are unlikely to be changed.

--- lib/Text/Xslate.pm
+++ lib/Text/Xslate.pm
@@ -4,7 +4,7 @@ use 5.008_001;
 use strict;
 use warnings;
 
-our $VERSION = '1.0000';
+our $VERSION = '1.0001';
 
 use Carp              ();
 use File::Spec        ();
@@ -522,7 +522,7 @@ Text::Xslate - Scalable template engine for Perl5
 
 =head1 VERSION
 
-This document describes Text::Xslate version 1.0000.
+This document describes Text::Xslate version 1.0001.
 
 =head1 SYNOPSIS
 
