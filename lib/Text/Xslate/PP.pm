c6164747895ad9a5b1c402e412566a22a6494f2d
Fuji, Goro <gfuji@cpan.org>
2011-02-08 12:59:45 +0900

    Checking in changes prior to tagging of version 1.0004.
    Changelog diff is:
    
    diff --git a/Changes b/Changes
    index 90aa59e..3bf9d0d 100644
    --- a/Changes
    +++ b/Changes
    @@ -1,5 +1,14 @@
     Revision history for Perl extension Text::Xslate
    
    +1.0004 2011-02-08 12:36:21
    +    NOTE: All the versions including this version has a bug that INCLUDE
    +          recursion doesn't work. It will be fixed in the next release.
    +    [BUG FIXES]
    +    * Fix a critical bug that warnings in templates could break the Perl stack
    +      (thanks to @tokuhirom for reporting it)
    +    * Fix a bug that method calls didn't work in PP version
    +      (thanks to @tokuhirom for reporting it)
    +
     1.0003 2011-02-05 11:54:45
         [NEW FEATURES]
         * For-loops allow overloaded objects. i.e. you can use Scalar::Defer

--- lib/Text/Xslate/PP.pm
+++ lib/Text/Xslate/PP.pm
@@ -3,7 +3,7 @@ package Text::Xslate::PP;
 use 5.008_001;
 use strict;
 
-our $VERSION = '1.0003';
+our $VERSION = '1.0004';
 $VERSION =~ s/_//; # for developpers versions
 
 BEGIN{
@@ -665,7 +665,7 @@ Text::Xslate::PP - Yet another Text::Xslate runtime in pure Perl
 
 =head1 VERSION
 
-This document describes Text::Xslate::PP version 1.0003.
+This document describes Text::Xslate::PP version 1.0004.
 
 =head1 DESCRIPTION
 
