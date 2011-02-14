1b2b067c8d8492514eea47a78a210e7e55d0d784
Fuji, Goro <gfuji@cpan.org>
2011-02-14 12:49:00 +0900

    Checking in changes prior to tagging of version 1.0008.
    Changelog diff is:
    
    diff --git a/Changes b/Changes
    index 1c038e9..fce8492 100644
    --- a/Changes
    +++ b/Changes
    @@ -1,6 +1,6 @@
     Revision history for Perl extension Text::Xslate
    
    -NEXT
    +1.0008 2011-02-14 12:46:45
         [BUG FIXES]
         * Fix docs

--- lib/Text/Xslate.pm
+++ lib/Text/Xslate.pm
@@ -4,7 +4,7 @@ use 5.008_001;
 use strict;
 use warnings;
 
-our $VERSION = '1.0007';
+our $VERSION = '1.0008';
 
 use Carp              ();
 use File::Spec        ();
@@ -512,7 +512,7 @@ Text::Xslate - Scalable template engine for Perl5
 
 =head1 VERSION
 
-This document describes Text::Xslate version 1.0007.
+This document describes Text::Xslate version 1.0008.
 
 =head1 SYNOPSIS
 
--- lib/Text/Xslate/PP.pm
+++ lib/Text/Xslate/PP.pm
@@ -3,7 +3,7 @@ package Text::Xslate::PP;
 use 5.008_001;
 use strict;
 
-our $VERSION = '1.0007';
+our $VERSION = '1.0008';
 $VERSION =~ s/_//; # for developpers versions
 
 BEGIN{
@@ -666,7 +666,7 @@ Text::Xslate::PP - Yet another Text::Xslate runtime in pure Perl
 
 =head1 VERSION
 
-This document describes Text::Xslate::PP version 1.0007.
+This document describes Text::Xslate::PP version 1.0008.
 
 =head1 DESCRIPTION
 
