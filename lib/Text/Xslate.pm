d437795e8c335d595f6d0d26fb523221b391987d
Fuji, Goro <gfuji@cpan.org>
2011-02-28 13:03:28 +0900

    Checking in changes prior to tagging of version 1.0009.
    Changelog diff is:
    
    diff --git a/Changes b/Changes
    index fce8492..2f2da4c 100644
    --- a/Changes
    +++ b/Changes
    @@ -1,5 +1,10 @@
     Revision history for Perl extension Text::Xslate
    
    +1.0009 2011-02-28 13:00:50
    +    [BUG FIXES]
    +    * Lock cache file operations with flock(2)
    +    * Fix doc issues
    +
     1.0008 2011-02-14 12:46:45
         [BUG FIXES]
         * Fix docs

--- lib/Text/Xslate.pm
+++ lib/Text/Xslate.pm
@@ -4,7 +4,7 @@ use 5.008_001;
 use strict;
 use warnings;
 
-our $VERSION = '1.0008';
+our $VERSION = '1.0009';
 
 use Carp              ();
 use Fcntl             ();
@@ -518,7 +518,7 @@ Text::Xslate - Scalable template engine for Perl5
 
 =head1 VERSION
 
-This document describes Text::Xslate version 1.0008.
+This document describes Text::Xslate version 1.0009.
 
 =head1 SYNOPSIS
 
