419726cbf5372e0a4986cf23779288d1a645cf1d
Fuji, Goro <gfuji@cpan.org>
2011-04-02 00:09:21 +0900

    Checking in changes prior to tagging of version 1.1001.
    Changelog diff is:

--- lib/Text/Xslate.pm
+++ lib/Text/Xslate.pm
@@ -4,7 +4,7 @@ use 5.008_001;
 use strict;
 use warnings;
 
-our $VERSION = '1.001';
+our $VERSION = '1.1001';
 
 use Carp              ();
 use Fcntl             ();
@@ -519,7 +519,7 @@ Text::Xslate - Scalable template engine for Perl5
 
 =head1 VERSION
 
-This document describes Text::Xslate version 1.001.
+This document describes Text::Xslate version 1.1001.
 
 =head1 SYNOPSIS
 
