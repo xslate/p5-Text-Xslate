cc195ac957ca7fcfbb7c1bb203183a9da4300d66
Fuji, Goro <gfuji@cpan.org>
2011-06-06 08:57:33 +0900

    Checking in changes prior to tagging of version 1.2004.
    Changelog diff is:
    
    diff --git a/Changes b/Changes
    index 66a3b05..fc73d9c 100644
    --- a/Changes
    +++ b/Changes
    @@ -1,5 +1,9 @@
     Revision history for Perl extension Text::Xslate
    
    +1.2004 2011-06-06 08:54:50
    +    [BUG FIXES]
    +    * Overriding builtins (introduced in 1.1005) didn't work
    +
     1.2003 2011-06-03 00:41:03
         [BUG FIXES]
         * Fix a miss-format in docs

--- lib/Text/Xslate.pm
+++ lib/Text/Xslate.pm
@@ -4,7 +4,7 @@ use 5.008_001;
 use strict;
 use warnings;
 
-our $VERSION = '1.2003';
+our $VERSION = '1.2004';
 
 use Carp              ();
 use Fcntl             ();
@@ -570,7 +570,7 @@ Text::Xslate - Scalable template engine for Perl5
 
 =head1 VERSION
 
-This document describes Text::Xslate version 1.2003.
+This document describes Text::Xslate version 1.2004.
 
 =head1 SYNOPSIS
 
