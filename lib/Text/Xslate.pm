5e7b0366febfbd717287a8f171244ce64e46db8d
Fuji, Goro <gfuji@cpan.org>
2011-07-23 18:00:43 +0900

    Checking in changes prior to tagging of version 1.3999_03.
    Changelog diff is:
    
    diff --git a/Changes b/Changes
    index 9fe6f03..b4a36d0 100644
    --- a/Changes
    +++ b/Changes
    @@ -1,10 +1,18 @@
     Revision history for Perl extension Text::Xslate
    
    +1.3999_03 2011-07-23 17:51:28
    +    [BUG FIXES]
    +    * [% FOR x IN ... %] no longer throw errors
    +      Using reserved words as variables is allowed if it's not ambiguous
    +
    +    [OTHER]
    +    * Files and directories have been cleaned up
    +
     1.3999_02 2011-07-23 13:31:09
    -   [DOCUMENT]
    -   * Improve FAQ and Cookbook
    -   * Mention to Text::Xslate::Syntax::HTMLTemplate and
    -     HTML::Template::Parser (thnaks to @shmorimo)
    +    [DOCUMENT]
    +    * Improve FAQ and Cookbook
    +    * Mention to Text::Xslate::Syntax::HTMLTemplate and
    +      HTML::Template::Parser (thnaks to @shmorimo)
    
     1.3999_01 2011-07-13 23:14:05
         The bytecode version is now 1.5.

--- lib/Text/Xslate.pm
+++ lib/Text/Xslate.pm
@@ -4,7 +4,7 @@ use 5.008_001;
 use strict;
 use warnings;
 
-our $VERSION = '1.3999_02';
+our $VERSION = '1.3999_03';
 
 use Carp              ();
 use Fcntl             ();
@@ -570,7 +570,7 @@ Text::Xslate - Scalable template engine for Perl5
 
 =head1 VERSION
 
-This document describes Text::Xslate version 1.3999_02.
+This document describes Text::Xslate version 1.3999_03.
 
 =head1 SYNOPSIS
 
--- lib/Text/Xslate/PP/Opcode.pm
+++ lib/Text/Xslate/PP/Opcode.pm
@@ -2,7 +2,7 @@ package Text::Xslate::PP::Opcode;
 use Any::Moose;
 extends qw(Text::Xslate::PP::State);
 
-our $VERSION = '1.3999_02';
+our $VERSION = '1.3999_03';
 
 use Carp ();
 use Scalar::Util ();
