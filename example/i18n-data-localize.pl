f73cc97cc52b8cf24db01798845055a7e22f3d54
Fuji, Goro <gfuji@cpan.org>
2011-11-06 23:06:28 +0900

    Add an example to use Data::Localize

---
+++ example/i18n-data-localize.pl
@@ -0,0 +1,43 @@
+#!perl
+use strict;
+use warnings;
+use utf8;
+
+use Text::Xslate;
+use Text::Xslate::Util qw(html_escape html_builder);
+use Data::Localize;
+use Data::Localize::Gettext;
+use FindBin qw($Bin);
+use Encode ();
+
+my $i18n = Data::Localize->new(
+    auto      => 1,
+    languages => ['ja'],
+);
+$i18n->add_localizer(
+    class => 'Gettext',
+    path  => "$Bin/locale/*.po",
+);
+
+my $xslate = Text::Xslate->new(
+    syntax   => 'TTerse',
+    function => {
+        l => sub {
+            return $i18n->localize(@_);
+        },
+        l_raw => html_builder {
+            my $format = shift;
+            my @args = map { html_escape($_) } @_;
+            return $i18n->locallize($format, @args);
+        },
+    },
+);
+
+my %param = (location => '<Tokyo>'); # user inputs
+my $body = $xslate->render_string(<<'TEMPLATE', \%param);
+[% l('Hello!<br />') %]
+[% l('I am in %1<br />', $location) %]
+TEMPLATE
+
+print Encode::encode_utf8($body);
+
