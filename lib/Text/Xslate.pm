7e24b24820d2447ceb8436a6c4ba5a77f1d1fcdf
Fuji, Goro <g.psy.va@gmail.com>
2012-03-28 09:43:07 +0900

    Checking in changes prior to tagging of version 1.5008.
    Changelog diff is:
    
    diff --git a/Changes b/Changes
    index a93364c..dec3ed0 100644
    --- a/Changes
    +++ b/Changes
    @@ -1,5 +1,11 @@
     Revision history for Perl extension Text::Xslate
    
    +1.5008 2012-03-28 09:39:45
    +    [BUG FIXES]
    +    - Fix a typo in an error messages (issues/53, thanks to wchristian)
    +    - Fix an undefined behavior that giving utf8-encoded *bytes* as
    +      params made mojibake in use of caches
    +
     1.5007 2011-11-17 19:43:10
         [BUG FIXES]
         * SWITCH without an END made infinate loops (reorted by ryochin)

--- lib/Text/Xslate.pm
+++ lib/Text/Xslate.pm
@@ -4,7 +4,7 @@ use 5.008_001;
 use strict;
 use warnings;
 
-our $VERSION = '1.5007';
+our $VERSION = '1.5008';
 
 use Carp              ();
 use Fcntl             ();
@@ -591,7 +591,7 @@ Text::Xslate - Scalable template engine for Perl5
 
 =head1 VERSION
 
-This document describes Text::Xslate version 1.5007.
+This document describes Text::Xslate version 1.5008.
 
 =head1 SYNOPSIS
 
