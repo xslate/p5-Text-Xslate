a0c6e58d240e2ee46ddce0c82b4a3e5cdc4578f4
Fuji, Goro <fuji.goro@dena.jp>
2012-07-13 19:40:20 +0900

    Checking in changes prior to tagging of version 1.5015.
    Changelog diff is:
    
    diff --git a/Changes b/Changes
    index 5cee538..51e7606 100644
    --- a/Changes
    +++ b/Changes
    @@ -1,5 +1,9 @@
     Revision history for Perl extension Text::Xslate
    
    +1.5015 2012-07-13 19:31:53
    +    [IMPROVEMENT]
    +    - Enhance extensibility for custom parsers (shmorimo)
    +
     1.5014 2012-06-26 19:41:20
         [IMPROVEMENT]
         - Add scalar::split() to Bridge::Star (tokuhirom)

--- lib/Text/Xslate.pm
+++ lib/Text/Xslate.pm
@@ -4,7 +4,7 @@ use 5.008_001;
 use strict;
 use warnings;
 
-our $VERSION = '1.5014';
+our $VERSION = '1.5015';
 
 use Carp              ();
 use Fcntl             ();
@@ -619,7 +619,7 @@ Text::Xslate - Scalable template engine for Perl5
 
 =head1 VERSION
 
-This document describes Text::Xslate version 1.5014.
+This document describes Text::Xslate version 1.5015.
 
 =head1 SYNOPSIS
 
