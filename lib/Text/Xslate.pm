683446a01e05c53bbe726b88171e1c030a11c4dd
Fuji, Goro <g.psy.va@gmail.com>
2012-10-15 00:09:37 +0900

    Checking in changes prior to tagging of version 1.5021.
    Changelog diff is:
    
    diff --git a/Changes b/Changes
    index 44efd41..69e457a 100644
    --- a/Changes
    +++ b/Changes
    @@ -1,5 +1,10 @@
     Revision history for Perl extension Text::Xslate
    
    +1.5021 2012-10-15 00:07:54
    +    [BUG FIXES]
    +    - Fix a segv that recursive call of render() may cause SETV
    +      thanks to doy for reporting this issue (isseu #65).
    +
     1.5020 2012-10-14 23:06:49
         [BUG FIXES]
         - Fix deploying issues reported at 1.5017, again.

--- lib/Text/Xslate.pm
+++ lib/Text/Xslate.pm
@@ -4,7 +4,7 @@ use 5.008_001;
 use strict;
 use warnings;
 
-our $VERSION = '1.5020';
+our $VERSION = '1.5021';
 
 use Carp              ();
 use File::Spec        ();
@@ -655,7 +655,7 @@ Text::Xslate - Scalable template engine for Perl5
 
 =head1 VERSION
 
-This document describes Text::Xslate version 1.5020.
+This document describes Text::Xslate version 1.5021.
 
 =head1 SYNOPSIS
 
--- lib/Text/Xslate/PP/Opcode.pm
+++ lib/Text/Xslate/PP/Opcode.pm
@@ -2,7 +2,7 @@ package Text::Xslate::PP::Opcode;
 use Any::Moose;
 extends qw(Text::Xslate::PP::State);
 
-our $VERSION = '1.5020';
+our $VERSION = '1.5021';
 
 use Carp ();
 use Scalar::Util ();
