cd75a6acde9b5a219b3a6548ecd6ce5d8502adb4
tokuhirom <tokuhirom@gmail.com>
2013-11-23 06:01:02 +0900

    minil migrate

---
+++ builder/MyBuilder.pm
@@ -0,0 +1,43 @@
+package builder::MyBuilder;
+use strict;
+use warnings;
+use utf8;
+use parent qw(Module::Build::XSUtil);
+
+sub new {
+    my ( $self, %args ) = @_;
+    $self->SUPER::new(
+        %args,
+        include_dirs => [qw(src/ .)],
+        xs_files => {
+            'src/Text-Xslate.xs' => 'lib/Text/Xslate.xs',
+        },
+        c_source => ['src/xslate_methods.c'],
+        generate_xshelper_h => 'xshelper.h',
+        meta_merge     => {
+            resources => {
+                homepage    => 'http://xslate.org/',
+                bugtracker  => 'https://github.com/xslate/p5-Text-Xslate/issues',
+                repository  => 'https://github.com/xslate/p5-Text-Xslate',
+                ProjectHome => 'https://github.com/xslate',
+                MailingList => 'http://groups.google.com/group/xslate',
+            }
+        },
+    );
+}
+
+sub ACTION_code {
+    my $self = shift;
+
+    system "$^X tool/opcode.PL        src/xslate_opcode.inc >xslate_ops.h";
+
+    require ExtUtils::ParseXS;
+    ExtUtils::ParseXS->new->process_file(
+        filename => 'src/xslate_methods.xs',
+        output => 'src/xslate_methods.c'
+    );
+
+    $self->SUPER::ACTION_code(@_);
+}
+
+1;
---
+++ cpanfile
@@ -0,0 +1,19 @@
+requires 'Data::MessagePack', '0.38';
+requires 'Mouse', '0.61';
+requires 'Scalar::Util', '1.14';
+requires 'XSLoader', '0.02';
+requires 'parent', '0.221';
+requires 'perl', '5.008001';
+
+on configure => sub {
+    requires 'Devel::PPPort', '3.19';
+    requires 'ExtUtils::MakeMaker', '6.59';
+    requires 'ExtUtils::ParseXS', '2.21';
+    requires 'File::Copy::Recursive';
+    requires 'Module::Build::XSUtil';
+};
+
+on test => sub {
+    requires 'Test::More', '0.88';
+    requires 'Test::Requires';
+};
