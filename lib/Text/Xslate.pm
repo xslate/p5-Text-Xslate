552654b49f7799a691119e28a3dc1d6f20aa66f2
Fuji, Goro (gfx) <gfuji@cpan.org>
2014-04-17 07:58:49 +0900

    Checking in changes prior to tagging of version 3.2.1.
    Changelog diff is:
    
    diff --git a/Changes b/Changes
    index 0caf3c9..9529468 100644
    --- a/Changes
    +++ b/Changes
    @@ -1,5 +1,9 @@
     Revision history for Perl extension Text::Xslate
    
    +3.2.1 2014-04-17 07:55:38+0900
    +    [BUG FIXES]
    +    - Fix #111 (by syohex in #113) inputting "0" made a wrong result
    +
     3.2.0 2014-04-04 07:39:59+0900
         [BUG FIXES]
         - Fix #107, #109 HashWithDefault should use "exists" (yappo, tokuhirom)

--- lib/Text/Xslate.pm
+++ lib/Text/Xslate.pm
@@ -4,7 +4,7 @@ use 5.008_001;
 use strict;
 use warnings;
 
-our $VERSION = '3.2.0';
+our $VERSION = '3.2.1';
 
 use Carp              ();
 use File::Spec        ();
@@ -663,7 +663,7 @@ Text::Xslate - Scalable template engine for Perl5
 
 =head1 VERSION
 
-This document describes Text::Xslate version 3.2.0.
+This document describes Text::Xslate version 3.2.1.
 
 =head1 SYNOPSIS
 
