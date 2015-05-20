c5ccd189beeeadb2aac5efdd87385e2189e0bccf
Yanick Champoux <yanick@babyl.dyndns.org>
2015-05-20 19:31:14 -0400

    more changes

--- lib/Text/Xslate/Runner.pm
+++ lib/Text/Xslate/Runner.pm
@@ -1,16 +1,22 @@
 package Text::Xslate::Runner;
+
 use Moo;
-use Moo::Util::TypeConstraints;
+
+use MooX::Types::MooseLike::Base qw(:all);  
 
 use List::Util     ();
 use File::Spec     ();
 use File::Basename ();
 use Getopt::Long   ();
 
+use Class::Load;
+
 {
     package
         Text::Xslate::Runner::Getopt;
+
     use Moo::Role;
+    use MooX::Types::MooseLike::Base qw(:all);  
 
     has cmd_aliases => (
         is         => 'ro',
@@ -29,9 +35,6 @@ my $getopt = Getopt::Long::Parser->new(
         no_auto_abbrev
     )],
 );
-
-my $Pattern = subtype __PACKAGE__ . '.Pattern', as RegexpRef;
-coerce $Pattern => from Str => via { qr/$_/ };
 
 my $getopt_traits = ['Text::Xslate::Runner::Getopt'];
 
@@ -125,8 +128,8 @@ has ignore => (
     documentation => 'Regular expression the process will ignore',
     cmd_aliases   => [qw(i)],
     is            => 'ro',
-    isa           => $Pattern,
-    coerce        => 1,
+    isa           => RegexpRef,
+    coerce        => sub { ref $_[0] ? $_[0] : qr/$_[0]/ },
     traits        => $getopt_traits,
 );
 
@@ -293,7 +296,7 @@ sub run {
         return;
     }
 
-    Moo::load_class($self->engine);
+    Class::Load::load_class($self->engine);
     my $xslate = $self->engine->new(%args);
 
     if($self->has_eval) {
@@ -447,7 +450,6 @@ sub _encode {
 }
 
 no Moo;
-no Moo::Util::TypeConstraints;
 __PACKAGE__->meta->make_immutable;
 
 __END__
