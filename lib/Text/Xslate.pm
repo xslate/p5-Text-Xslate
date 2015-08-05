26f35a8e993ae7f8f1a41ec5097c603cbee31b61
Syohei YOSHIDA <syohex@gmail.com>
2015-08-05 18:51:29 +0900

    Bump up version and update Changes

--- lib/Text/Xslate.pm
+++ lib/Text/Xslate.pm
@@ -4,7 +4,7 @@ use 5.008_001;
 use strict;
 use warnings;
 
-our $VERSION = '3.3.4';
+our $VERSION = '3.3.5';
 
 use Carp              ();
 use File::Spec        ();
@@ -668,7 +668,7 @@ Text::Xslate - Scalable template engine for Perl5
 
 =head1 VERSION
 
-This document describes Text::Xslate version 3.3.4.
+This document describes Text::Xslate version 3.3.5.
 
 =head1 SYNOPSIS
 
